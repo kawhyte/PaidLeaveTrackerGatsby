181d2b1dd3b143290d5e6088c0b32154440b43fa
Kenny <kawhyte@gmail.com>
2020-04-02 14:43:40 -0700

    getting a slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,18 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path')
 
- // You can delete this file if you're not using it
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNode, createNodeField } = actions
+ 
+  if (node.internal.type === 'MarkdownRemark') { 
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+
+    createNodeField({
+        node,
+        name: 'slug',
+        value:slug
+    })
+  }
+}
