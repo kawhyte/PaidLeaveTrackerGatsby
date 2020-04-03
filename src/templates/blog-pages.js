6bae6aa24f76c1b3e8025e0452bd6bafc6851b66
Kenny <kawhyte@gmail.com>
2020-04-03 09:28:06 -0700

    blog pages

--- src/templates/blog-pages.js
+++ src/templates/blog-pages.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import Layout from "../components/layout"
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+const BlogPage = (props) => {
+  return (
+    <Layout>
+      <article class="pa3 pa5-ns w-50-ns">
+  <h1 class="f3 f2-m f1-l bb bw2 pb3">{props.data.markdownRemark.frontmatter.title}</h1>
+  <h2 class="f3 mid-gray lh-title">
+            An excerpt from the Form of the Book by Jan Tschichold
+          </h2>
+          <time className="f6 ttu tracked gray">{props.data.markdownRemark.frontmatter.date}</time>
+  <p class="measure lh-copy" dangerouslySetInnerHTML= {{ __html: props.data.markdownRemark.html }}></p>
+ 
+</article>
 
 
-const BlogPage = () => {
 
-return(
-<Layout>
-Blog Template
-
-</Layout>
-
-
-)
-
+    </Layout>
+  )
 }
 
 export default BlogPage
