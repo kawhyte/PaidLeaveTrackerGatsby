5cfa027112a409396063b752b5cd40e96ab3a039
Kenny <kawhyte@gmail.com>
2020-04-07 09:48:00 -0700

    added new and major update section

--- src/components/card.js
+++ src/components/card.js
@@ -1,85 +1,44 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import addDays from 'date-fns/addDays'
+import  differenceInDays from 'date-fns/differenceInDays'
+import  parseJSON from 'date-fns/parseJSON'
 
 // let differenceInCalendarDays = require("date-fns/differenceInCalendarDays");
-// let add = require("date-fns");
-// var formatDistance = require("date-fns");
-// var parseJSON = require("date-fns/parseJSON");
+// let date = require("date-fns");
+// let add = require("date-fns/add")
+// //var addDays = require('date-fns/addDays')
+// // var formatDistance = require("date-fns");
+//  let parseJSON = require("date-fns/parseJSON");
 
 const Card = ({ title, identifier, jurisdiction, createdAt, sources, actions  }) => {
 
+ ////LOGIC TO CHECK IF BILL IS NEW //////
+let futureDate = addDays(new Date(Date.now()), 7)
+ let billDateDifference = differenceInDays(
+    futureDate,
+    new Date(parseJSON("2020-04-03 11:02:32.289671+00:00"))
+  );
 
+ ///LOGIC TO CHECK IF BILL IS IMPORTANT //////
+ let   isMajorUpdate = actions.some(
+      value =>
+        value.classification.includes("withdrawal") ||
+        value.classification.includes("failure") ||
+        value.classification.includes("became-law") ||
+        value.classification.includes("amendment-passage") ||
+        value.classification.includes("amendment-failure") ||
+        value.classification.includes("executive-receipt") ||
+        value.classification.includes("executive-signature") ||
+        value.classification.includes("executive-veto") ||
+        value.classification.includes("executive-veto-line-item") ||
+        value.classification.includes("veto-override-passage") ||
+        value.classification.includes("veto-override-failure") ||
+        Object.values(value.description).includes("governor") ||
+        Object.values(value.description).includes("executive") 
+    );
 
-//   // Client-side Runtime Data Fetching
-//   const [createdAt2, setStarsCount] = useState(false)
-//   useEffect(() => {
-//   ////LOGIC TO CHECK IF BILL IS NEW //////
-//    var futureDate = add.add (new Date(Date.now()), {
-//     days: 1,
-//     hours: 2,
-//     minutes: 9,
-//     seconds: 30
-//   });
-//   // console.log("future date ", futureDate)
-//   // console.log("createdAt2 ", createdAt2)
-//   // console.log("add.parseJSON(createdAt) ", add.parseJSON(createdAt2))
-//   var result = add.differenceInCalendarDays(
-//     futureDate,
-//     new Date(add.parseJSON(createdAt))
-//   );
-//   console.log("result ", result)
-
-//   //   return () => {
-//   //     cancel();
-//   //   };
-// if (result > 14) {
-  
-//     console.log("New?? ", identifier) 
- 
-//   }
- 
-//   }, [])
-
-
-//  console.log("RESULTTTTS ", createdAt2)
-
-
-
-
-
-
-
-
-
-
-
-   ////LOGIC TO CHECK IF BILL IS NEW //////
-  //  var futureDate = add (new Date(Date.now()), {
-  //   days: 1,
-  //   hours: 2,
-  //   minutes: 9,
-  //   seconds: 30
-  // });
-
-  // var result = differenceInCalendarDays(
-  //   futureDate,
-  //   new Date(parseJSON(createdAt))
-  // );
-
-  // console.log("future date ", futureDate)
-  // console.log("created date  - future date ", result)
-
-  // if (result > 14) {
-  //   console.log("New?? ", identifier) 
- 
-  // }
-
-// console.log("Today ", (Date.now()))
-// console.log("createdAt ", (new Date(createdAt)))
-
-
-
-
+ console.log("importantValue ", jurisdiction, identifier, isMajorUpdate)
 
 
   return (
@@ -92,7 +51,7 @@ const Card = ({ title, identifier, jurisdiction, createdAt, sources, actions  })
     <img src={"https://res.cloudinary.com/babyhulk/image/upload/v1584505244/flags/Flag_of_" + jurisdiction}  alt= {"Flag of" + jurisdiction} className="br2 h3 w3 dib" title={"Flag of " + jurisdiction} ></img>
   <h1 className="f4">{jurisdiction} - {identifier}</h1>
     <span>
-      data.isBillNew? '<a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy bg-light-yellow">New</a>': ""data.isLastUpdateImportant ? '<a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy bg-washed-green">Major Update</a>': ""
+   {billDateDifference > -7714 ? <a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy bg-light-yellow">New</a> : " "} { isMajorUpdate ? <a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy bg-washed-green">Major Update</a> : " "}
    </span>
     <hr className="mw3 bb bw1 b--black-10" />
     </div>
@@ -100,46 +59,41 @@ const Card = ({ title, identifier, jurisdiction, createdAt, sources, actions  })
     </div>
 
     <div>
-        <article className="w-100 pa0">
+        <article className="w-100 pa0 bg-black-10">
 
             <div className=" bg-white black w-100 mt1 ph0 pv0">
                 <div className="w-100 pb3 bb b--light-gray flex items-center justify-between">
 
                         <div className="pt2 f3-m fw5 white">
                                 
-                        <h3 className="tc f4 f4-m measure-narrow lh-title mv0">
+                        <h3 className="tc f4 f4-m measure-narrow lh-title mv0 bg-black-10 center">
                             <span className="$
                               data.statusColor
-                             lh-copy black pa1 tracked-tight">
+                             lh-copy black pa1 tracked-tight tc">
                              {actions[actions.length - 1].description}
                             </span>
                           </h3>
                    
-                            <div className="pt3 pl3 pr3 w-100 dt dt--fixed">
-                       
-                                <div className={"dtc h1 black " +
-
-actions.length ? "bg-green" : "bg-light-gray"  + "br1 br--left tc pr3"} 
-                                 
-                                >
-                                    <small>Introduced</small></div>
-                                <div className="dtc h1 black  
-                                 br1 br--left tc" >
-                                    <small>House</small></div>
-                                <div className="dtc h1 black 
-                              
-                                br1 br--left tc" >
-                                    <small>Senate</small></div>
-                                <div className="dtc h1 black
-                              
-                                 br1 br--left tc" >
-                                    <small>Gov</small></div>
-                                <div className="dtc h1 bg-white o-30 br1 br--right"></div>
-                            </div>
+                            <div className="pt3 pl3 pr3 w-100 dt dt--fixed bg-black-50 flex items-center justify-between">
+                            <span> <a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy tc bg-light-yellow" style={{width: 100}}>Introduced</a></span>
+                            <span>&#8594;</span>
+                            <span> <a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy bg-light-yellow tc "style={{width: 100}}>House</a></span>
+                            <span>&#8594;</span>
+                            <span> <a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy bg-light-yellow tc "style={{width: 100}}>Senate</a></span>
+                            <span>&#8594;</span>
+                            <span> <a className="f6 grow no-underline br-pill ph2 pv1 mb2 dib navy bg-light-yellow tc "style={{width: 100}}>Governor</a></span>
+                        
 
 
 
-                            <div className="pt1 pl3 pr3 w-100 dt dt--fixed">
+                           </div>
+                     
+                                
+                        
+
+
+
+                            <div className="pt1 pl3 pr3 w-100 dt dt--fixed bg-red">
                        
                                 <div className="dtc h1 black  br1 br--left tc" style={{width: 50}}>
                                     <small className= "f7 gray">{actions.date =
