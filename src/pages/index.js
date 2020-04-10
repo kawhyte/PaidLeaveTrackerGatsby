b56f2e895fbb94ba4a1346c06c2fc5b98720cdd0
Kenny <kawhyte@gmail.com>
2020-04-09 20:29:35 -0700

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,49 +5,13 @@ import CardList from "../components/cardList"
 
 const IndexPage = () => {
 
-  // const [bill, setBill] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   let cancel;
-  //   axios
-  //     .get("https://paidleavetracker.herokuapp.com/track", {
-  //       cancelToken: new axios.CancelToken(c => (cancel = c))
-  //     })
-  //     .then(res => {
-  //       // setBill(res.data.map(bill => bill.bill_id));
-  //       setBill(res.data);
-  //       setLoading(false);
-  //     });
-
-  //   return () => {
-  //     cancel();
-  //   };
-  // }, [])
-
-
-
-
-
-
-
   return ( 
-  <div  className="Roboto">
-
-
-
-
+<div  className="Roboto">
 <Layout> 
 <Hero /> 
-
-<CardList> </CardList>
-  
+<CardList /> 
 </Layout>
-   
-  </div>
-
-
+</div>
 )
   
 }
