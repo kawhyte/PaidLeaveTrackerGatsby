0194e25493577fb4aa776179bf2612fe29c8b5ea
Kenny <kawhyte@gmail.com>
2020-04-09 21:11:14 -0700

    tem

--- src/templates/blog-pages.js
+++ src/templates/blog-pages.js
@@ -37,18 +37,17 @@ const BlogPage = (props) => {
   }
   return (
     <Layout>
-      <article class="pa3 pa5-ns w-50-ns">
-        <h1 class="f3 f2-m f1-l bb bw2 pb3">
+      <article >
+        <h1 >
           {props.data.contentfulBlogPost.title}
         </h1>
-        <h2 class="f3 mid-gray lh-title">
+        <h2 >
           An excerpt from the Form of the Book by Jan Tschichold
         </h2>
-        <time className="f6 ttu tracked gray">
+        <time >
           {props.data.contentfulBlogPost.date}
         </time>
         {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
-        {/* <p class="measure lh-copy" dangerouslySetInnerHTML= {{ __html: props.data.markdownRemark.html }}></p> */}
       </article>
     </Layout>
   )
