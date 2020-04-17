9ba27034af3ab22b97ecd0d23bd24bcf63161df5
Kenny <kawhyte@gmail.com>
2020-04-17 09:50:12 -0700

    updated table

--- src/components/table.js
+++ src/components/table.js
@@ -82,6 +82,17 @@ const Table = ({ title, identifier, jurisdiction, actions, sources  }) => {
 
         <tr>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+              <div className="">
+              <div className="flex-shrink-0 w-10 h-10 mr-3">
+                          <img className="w-full h-full rounded-full"
+                              src={"https://res.cloudinary.com/babyhulk/image/upload/v1584505244/flags/Flag_of_" + jurisdiction +".svg"} alt={"Flag of" + jurisdiction}
+                               />
+              </div>
+                  <p className="text-gray-900 whitespace-no-wrap">{jurisdiction} | <span className="bg-blue-100 p-1"> {identifier}</span></p>
+              
+                </div>
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
             <div className="">
                 {/* <div className="flex-shrink-0 w-10 h-10">
                     <img className="w-full h-full rounded-full"
@@ -89,22 +100,11 @@ const Table = ({ title, identifier, jurisdiction, actions, sources  }) => {
                          />
                 </div> */}
                 <div className="ml-3">
-                    <p className="text-gray-900 wrap">
+                    <p className="text-gray-900  wrap">
                     {sentenceCase(title)} 
                     </p>
                 </div>
             </div>
-        </td>
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <div className="">
-        <div className="flex-shrink-0 w-10 h-10 mr-3">
-                    <img className="w-full h-full rounded-full"
-                        src={"https://res.cloudinary.com/babyhulk/image/upload/v1584505244/flags/Flag_of_" + jurisdiction +".svg"} alt={"Flag of" + jurisdiction}
-                         />
-        </div>
-            <p className="text-gray-900 whitespace-no-wrap">{jurisdiction} | {identifier}</p>
-        
-          </div>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white-200 text-sm">
 
@@ -113,10 +113,10 @@ const Table = ({ title, identifier, jurisdiction, actions, sources  }) => {
   <div className="inline-flex items-center bg-gray-400">
 </div>
         </td>
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <td className="w-auto border-b border-gray-200 bg-white text-sm">
 
 
-<div className="flex ml-4" > 
+<div className="flex ml-3" > 
 
 {isBillNew (getBillIntroduction(actions)) === true ? <span className="text-sm font-medium bg-blue-100 py-1 px-2 rounded text-blue-500 align-middle">New</span> :" "}
 {isBillSignedByGovornor(actions) ?  <span className="text-sm font-medium bg-green-100 py-1 px-2 rounded text-green-500 align-middle"> Passed</span> :" "}
@@ -128,14 +128,14 @@ const Table = ({ title, identifier, jurisdiction, actions, sources  }) => {
 
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white  text-sm">
-            <p className="text-gray-900 whitespace-pre w-auto ">
+            <p className="text-gray-900 w-56 ">
             <time>{format(new Date(actions[0].date),'LLL dd, yyyy')}</time>  
-            <p className="bg-blue-100 mb-3 f6 db pv2 wrap  ">{sentenceCase(actions[0].description)}</p>
             </p>
+             <p className=" f6 w-auto inline-block wrap">{sentenceCase(actions[0].description)}</p> 
         </td>
 
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-            <p className="text-gray-900 whitespace-no-wrap">
+            <p className="text-gray-900 w-auto whitespace-pre">
 
     {sources.map((u, i)=>{
  
--- src/components/tableList.js
+++ src/components/tableList.js
@@ -72,33 +72,33 @@ const TableList = function (v) {
             
 
             <div className="-mx-4 sm:-mx-8 px-4 sm:px-5 py-4 overflow-x-auto">
-                <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                    <table className="min-w-full leading-normal">
+                <div className="inline-block  shadow rounded-lg overflow-hidden">
+                    <table className="table-fixed ">
       
                     <thead>
                             <tr>
                             <th
-                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    bill title
-                                </th>
-                                <th
-                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    className="w-1/6 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                     State/Bill ID
                                 </th>
                                 <th
-                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    className="w-2/6 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    bill title
+                                </th>
+                                <th
+                                    className="w-2/6 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                     Bill Progression
                                 </th>
                                 <th
-                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    className="w-1/6 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                     Bill Status
                                 </th>
                                 <th
-                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    className="w-1/6 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                     Last Update
                                 </th>
                                 <th
-                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    className="w-1/6 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                     Sources
                                 </th>
                              
