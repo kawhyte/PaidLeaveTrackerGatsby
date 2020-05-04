0d381b0646e18be108a3525715960e9c4d69c7ed
Kenny <kawhyte@gmail.com>
2020-05-04 15:15:28 -0700

    .

--- src/components/common/billSource.jsx
+++ src/components/common/billSource.jsx
@@ -5,7 +5,7 @@ function billSource({sources}) {
         <>
         <ul className=" flex text-blue-700 list-inside">
         {sources.map((link, i)=>{
-
+ 
             return (<li key={i}> <a href={link.url} target="_blank" rel="noopener" rel="noopener noreferrer" className="mr-3 flex-auto text-sm font-medium bg-blue-100 py-1 px-2 rounded text-blue-500 align-middle no-underline"> {i+1}</a> </li>)
             
                 })}
