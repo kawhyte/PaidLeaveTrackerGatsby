30ab9a50fdeabed8a3e8760d3238cacfb2fb618a
Kenny <kawhyte@gmail.com>
2020-05-04 15:39:36 -0700

    Updated hero image

--- src/components/hero.js
+++ src/components/hero.js
@@ -47,7 +47,7 @@ const Hero = () => {
     </div>
   </div>
   <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-    <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src= {svg} alt="" />
+    <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://res.cloudinary.com/babyhulk/image/upload/f_auto/v1588631591/hero-image/family-green.svg" alt="" />
   </div>
 </div>
 
