cffc3719035289ad10871809ece7e66f0e9d5abc
Kenny <kawhyte@gmail.com>
2020-05-04 18:07:14 -0700

    select access

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -8,15 +8,16 @@ const ListGroup = props => {
     
     return  (
 <>
-        <label htmlFor="filter-value">
-        <select id="filter-value" onChange ={(e)=> onChange(e )}
+        <label  id="lbl-main-menu-mob"></label>
+        <select id="lbl-main-menu-mob" aria-label="lbl-main-menu-mob" type="text" name="filter" onChange ={(e)=> onChange(e )}
             className="sm:ml-3 appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 sm:border-r border-r border-l border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
             <option defaultValue="all" value="all" >All</option>
             <option value="new">New bills</option>
             <option value="major">Major Update</option>
             <option value="passed">Signed by Gov</option>
             <option value="failed">Failed bills</option>
-        </select></label>
+        </select>
+        
         <div
             className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
