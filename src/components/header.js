b38280a57ea1933b3b415b21218109259f5a3188
Kenny <kawhyte@gmail.com>
2020-06-02 19:50:33 -0700

    revert

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import IdentityModal, { useIdentityContext } from "react-netlify-identity-widget"
-import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
 
 
 const Header = ({ siteTitle }) => {
-
-  const identity = useIdentityContext()
-  const [dialog, setDialog] = React.useState(false)
-  const name =
-    (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.name) || "NoName"
-
-  console.log(JSON.stringify(identity))
-  const isLoggedIn = identity && identity.isLoggedIn
 
   return (
     <>
