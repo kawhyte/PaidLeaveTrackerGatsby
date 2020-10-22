7c66f677424c361d83b55b901e3825a6c477570d
Kenny Whyte <kawhyte@gmail.com>
2020-10-22 13:41:12 -0700

    removed blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-const path = require('path')
+//const path = require('path')
 
 // module.exports.onCreateNode = ({ node, actions }) => {
 //   const { createNode, createNodeField } = actions
@@ -22,9 +22,9 @@ const path = require('path')
 
 
 
-module.exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`./src/templates/blog-pages.js`)
+// module.exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve(`./src/templates/blog-pages.js`)
 
 
 
@@ -33,28 +33,28 @@ module.exports.createPages = async ({ graphql, actions }) => {
 
 
   
-  const res = await graphql(`
-  query{
-    allContentfulBlogPost{
-      edges {
-        node{
+//   const res = await graphql(`
+//   query{
+//     allContentfulBlogPost{
+//       edges {
+//         node{
        
-          slug
+//           slug
           
-        }
-      }
-    }
-  }
-  `)
+//         }
+//       }
+//     }
+//   }
+//   `)
 
-  res.data.allContentfulBlogPost.edges.forEach((edge) => {
+//   res.data.allContentfulBlogPost.edges.forEach((edge) => {
 
-    createPage({
-      component: blogPostTemplate,
-      path: `/blog/${edge.node.slug}`,
-      context: {
-          slug: edge.node.slug
-      }
-    })
-  })
-}
+//     createPage({
+//       component: blogPostTemplate,
+//       path: `/blog/${edge.node.slug}`,
+//       context: {
+//           slug: edge.node.slug
+//       }
+//     })
+//   })
+// }
