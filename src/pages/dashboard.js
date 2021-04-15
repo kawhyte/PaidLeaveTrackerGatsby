870f9efe0a95bc88d45cf53cfa2de80e673cb573
Kenny Whyte <kawhyte@gmail.com>
2021-04-15 16:29:47 -0700

    .

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Layout from '../components/layout'
-import Navbar from '../components/Navbar'
+
+
 import DisplayList from '../components/displayList'
 
 const Dash = () => {
--- src/pages/employment.js
+++ src/pages/employment.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Layout from '../components/layout'
-import Navbar from '../components/Navbar'
 import DisplayList from '../components/displayList'
 
 const Employment = () => {
